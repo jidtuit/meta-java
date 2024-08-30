d595b3b5748d4e0764f83242fe107d573277c9c4
Jose Ignacio Dominguez <jid@JIDpersonal.lan>
2024-08-30 14:15:26 +0200

    Remove unused imports

--- lib/src/main/java/org/jid/metajava/model/ClassMeta.java
+++ lib/src/main/java/org/jid/metajava/model/ClassMeta.java
@@ -1,7 +1,5 @@
 package org.jid.metajava.model;
 
-import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 
 public record ClassMeta(String name, ClassType type, Set<MethodMeta> methods, Set<AnnotationMeta> annotations, String packageName,
